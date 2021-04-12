715a0cf705844e3ab8280df69ab2b44451e3c7b9
gnguralnick <gnguralnick@gmail.com>
2021-04-12 17:28:13 -0400

    Updated visualize_convergence to correctly graph separate lines for each article
    All of the lines are horizontal, so there may still be a problem with either visualize_convergence or calculate_pagerank_manual

--- src/visualize.py
+++ src/visualize.py
@@ -133,25 +133,22 @@ def visualize(x_values: list, y_values: list, sizes: Union[list, int], colours: 
 def visualize_convergence(graph: nx.Graph) -> None:
     """Visualize the convergence of the manual PageRank algorithm.
     Note that the results of this algorithm differ slightly from the NetworkX implementation.
-
-    INCOMPLETE AND CURRENTLY NOT NEEDED: manual pagerank implementation converges in 1 iteration"""
+    """
     all_page_ranks = algorithms.calculate_pagerank_manual(graph)
-    print(len(all_page_ranks))
     article_convergences = dict.fromkeys(all_page_ranks[0].keys(), [])
     for article in article_convergences:
-        for iteration in all_page_ranks:
-            article_convergences[article].append(iteration[article])
+        article_convergences[article] = [iteration[article] for iteration in all_page_ranks]
     times = [i for i in range(len(all_page_ranks))]
-    lines = []
+    fig = Figure()
     for article in article_convergences:
         article_scatter = Scatter(x=times,
                                   y=article_convergences[article],
-                                  mode='lines+markers')
-        lines.append(article_scatter)
-    fig = Figure(data=lines)
-    fig.update_layout(showlegend=False, title=graph.graph['category'] + ' PageRank convergence')
-    fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
-    fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
+                                  mode='lines+markers',
+                                  name=article)
+        fig.add_trace(article_scatter)
+    fig.update_layout(showlegend=True, title=graph.graph['category'] + ' PageRank convergence')
+    fig.update_xaxes(showgrid=True, zeroline=True, visible=True)
+    fig.update_yaxes(showgrid=True, zeroline=True, visible=True)
     fig.show()
 
 
@@ -180,4 +177,4 @@ if __name__ == '__main__':
     import algorithms
     algorithms.assign_pagerank(test_graph)
     visualize_convergence(test_graph)
-    visualize_pagerank(test_graph, arrows=True)
+    # visualize_pagerank(test_graph, arrows=False)
